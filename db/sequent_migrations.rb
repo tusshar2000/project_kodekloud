6670d670e91411068db8d58f36c301f26ccbbb0e
Tusshar Laddha <tussharladdha@Tusshars-MacBook-Pro.local>
2024-04-28 15:28:47 +0530

    seq mig

--- db/sequent_migrations.rb
+++ db/sequent_migrations.rb
@@ -25,3 +25,31 @@ class SequentMigrations < Sequent::Migrations::Projectors
     }
   end
 end
+
+# VIEW_SCHEMA_VERSION = 5
+
+# class SequentMigrations < Sequent::Migrations::Projectors
+#   def self.version
+#     VIEW_SCHEMA_VERSION
+#   end
+
+#   def self.versions
+#     {
+#       '1' => [
+
+#       ],
+#       '2' => [
+#         Course::CourseProjector
+#       ],
+#       '3' => [
+#         Lesson::LessonProjector
+#       ],
+#       '4' => [
+#         UserCourse::UserCourseProjector
+#       ],
+#       '5' => [
+#         UserLessonCompleted::UserLessonCompletedProjector
+#       ]
+#     }
+#   end
+# end
